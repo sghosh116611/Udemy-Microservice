339142815af40c36eb555acabbc3b8aff65ae0e3
Soumojit Ghosh <ghoshsoumojit53@gmail.com>
2023-11-19 12:28:35 +0530

    Setting up API Gateway

---
+++ ApiGateway/src/test/java/com/personalproject/photoapp/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalproject.photoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
